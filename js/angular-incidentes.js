9b2b423d2ff056b01843942425b07b9f202b99a2
crearee <54912674+crearee@users.noreply.github.com>
2019-09-04 18:20:39 -0500

    Add files via upload

--- js/angular-incidentes.js
+++ js/angular-incidentes.js
@@ -1,67 +1,7 @@
 var app = angular.module('piscina_segura', []);
 app.controller('ctrl_body', ['$scope', '$http', function ($scope, $http) {
 
-        //----- Almacena los booleanos del formulario de mantenimiento -----
-        $scope.frm_incidente = {"cerrar":false};
-        
-        //----- Actualiza el estado de las labores de mantenimiento -----
-         $scope.enviar_incidente = function(incidente) {
-             console.log(incidente);
-         };
-
-        $(window).on('load', function () {
-            /* sparklines */
-            $(".dynamicsparkline").sparkline([5, 6, 7, 2, 0, 4, 2, 5, 6, 7, 2, 0, 4, 2, 4], {
-                type: 'bar',
-                height: '25',
-                barSpacing: 2,
-                barColor: '#a9d7fe',
-                negBarColor: '#ef4055',
-                zeroColor: '#ffffff'
-            });
-
-            /* gauge chart circular progress */
-            $('.progress_profile1').circleProgress({
-                fill: '#169cf1',
-                lineCap: 'butt'
-            });
-            $('.progress_profile2').circleProgress({
-                fill: '#f4465e',
-                lineCap: 'butt'
-            });
-            $('.progress_profile4').circleProgress({
-                fill: '#ffc000',
-                lineCap: 'butt'
-            });
-            $('.progress_profile3').circleProgress({
-                fill: '#00c473',
-                lineCap: 'butt'
-            });
-            $('.progress_profile5').circleProgress({
-                fill: '#ffffff',
-                lineCap: 'butt'
-            });
-
-            /*Swiper carousel */
-            var mySwiper = new Swiper('.swiper-container', {
-                slidesPerView: 2,
-                spaceBetween: 0,
-                autoplay: {
-                    delay: 1500,
-                    disableOnInteraction: false,
-                },
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                }
-            });
-            /* tooltip */
-            $(function () {
-                $('[data-toggle="tooltip"]').tooltip();
-            });
-        });
-
-
+        $scope.frm_incidentes = {};
 
 
     }]);
